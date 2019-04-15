9d27d471c66e1829007aa4338b554dae54410929
Mustafa ALKAN <mustafaalkan64@gmail.com>
2019-04-15 16:06:39 +0300

    Bug Fix

--- OmdbApi.DAL/Entities/Ratings.cs
+++ OmdbApi.DAL/Entities/Ratings.cs
@@ -11,6 +11,6 @@ namespace OmdbApi.DAL.Entities
         public string Source { get; set; }
         public string Value { get; set; }
         public int MovieId { get; set; }
-        public Movie Movie { get; set; }
+        //public Movie Movie { get; set; }
     }
 }
