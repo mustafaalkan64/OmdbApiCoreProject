1c25656da41364ba853ac46e560b48e69e11db00
Mustafa ALKAN <mustafaalkan64@gmail.com>
2019-04-15 13:16:57 +0300

    Rating Seperated From Movie
    Rating Seperated From Movie

--- OmdbApi.DAL/Entities/Movie.cs
+++ OmdbApi.DAL/Entities/Movie.cs
@@ -43,14 +43,5 @@ namespace OmdbApi.DAL.Entities
         public string Website { get; set; }
         public string Response { get; set; }
     }
-    public class Rating
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        public string Source { get; set; }
-        public string Value { get; set; }
-        public int MovieId { get; set; }
-        public Movie Movie { get; set; }
-    }
+    
 }
---
+++ OmdbApi.DAL/Entities/Ratings.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OmdbApi.DAL.Entities
+{
+    public class Rating
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Source { get; set; }
+        public string Value { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
