53808523568c72562279870aee29de807ac83f79
mustafa alkan <mustafaalkan64@gmail.com>
2019-05-05 20:57:37 +0300

    Logon Page Added

---
+++ OmdbApi.Web/ClientApp/components/logon/logon.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import axios from 'axios';
+import { APIService } from '../../APIService';
+const apiService = new APIService();
+
+
+@Component
+export default class LogonComponent extends Vue {
+    
+}
--- OmdbApi.Web/ClientApp/components/movies/movies.ts
+++ OmdbApi.Web/ClientApp/components/movies/movies.ts
@@ -24,7 +24,7 @@ interface ratings {
 }
 
 @Component
-export default class MoviesController extends Vue {
+export default class MoviesComponent extends Vue {
     // token will get from login method
     token: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtdXN0YWZhMiIsImp0aSI6IjEwZTE3ZmJhLTUyNjItNDZlYS1iNWJkLWMzZTFiMGEzZWVhZSIsImVtYWlsIjoibXVzdGFmYTJAZ21haWwuY29tIiwiVXNlcklkIjoiNCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoibXVzdGFmYTIiLCJleHAiOjE1NTcwNjI2MDEsImlzcyI6Imh0dHBzOi8vZ2l0aHViLmNvbS9tdXN0YWZhYWxrYW42NC9PbWRiQXBpQ29yZVByb2plY3QiLCJhdWQiOiJodHRwczovL2dpdGh1Yi5jb20vbXVzdGFmYWFsa2FuNjQvT21kYkFwaUNvcmVQcm9qZWN0In0.vh0GurWmeMK9o82zzeevt6x5Ksdqpv-zc_UFG7iP71w";
     movie: movie = <movie>{
@@ -63,6 +63,11 @@ export default class MoviesController extends Vue {
             })
             .catch((error: any) => {
                 console.log(error);
+                if (error.response && error.response.status === 401) {
+                    window.location.href = "logon";
+                } else {
+                    // Handle error however you want
+                }
             });
     }
 
